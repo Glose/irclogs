074b3d25541b4cdb01b027f58a8d39538d63c5b2
Julien Chaumond <chaumond@gmail.com>
2013-04-30 14:33:54 +0200

    Log messages

---
+++ main.js
@@ -0,0 +1,21 @@
+
+var irc = require('irc');
+
+var client = new irc.Client('irc.freenode.net', 'irclogs', {
+	channels: ['#laravel'],
+});
+
+
+client.addListener('message', function (from, to, message) {
+	console.log(from + ' => ' + to + ': ' + message);
+});
+
+
+////////////////////////////////////////////////////////////////////
+/////////////////////////// ERROR HANDLING /////////////////////////
+////////////////////////////////////////////////////////////////////
+
+client.addListener('error', function(message) {
+	console.log('error: ', message);
+});
+
