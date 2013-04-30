e8f79b5bad7a008cb120316e8beee624e9247899
Julien Chaumond <chaumond@gmail.com>
2013-04-30 15:51:31 +0200

    Feed data into Mongo
    @Anahkiasen @arthurdarcet Code review please

--- main.js
+++ main.js
@@ -1,25 +1,33 @@
 
 var irc = require('irc');
+var mongojs = require('mongojs');
+
 
 var client = new irc.Client('irc.freenode.net', 'irclogs', {
 	channels: ['#laravel'],
 });
 
+var logs = mongojs('irclogs').collection('logs');
+
 
 client.addListener('message', function (nick, to, text, message) {
 	console.log(nick + ' => ' + to + ': ' + text);
+	logs.save({type: 'message', nick: nick, text: text}, {w: 0});
 });
 
 client.addListener('join', function (channel, nick, message) {
 	console.log('JOIN', channel, nick, message);
+	logs.save({type: 'join', nick: nick}, {w: 0});
 });
 
 client.addListener('part', function (channel, nick, reason, message) {
 	console.log('PART', channel, nick, reason, message);
+	logs.save({type: 'part', nick: nick, reason: reason}, {w: 0});
 });
 
 client.addListener('quit', function (nick, reason, channels, message) {
 	console.log('QUIT', nick, reason, channels, message);
+	logs.save({type: 'quit', nick: nick, reason: reason}, {w: 0});
 });
 
 
