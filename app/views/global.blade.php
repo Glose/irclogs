52bd409064b6326c322bf63a2b76193d4de5f4da
Julien Chaumond <chaumond@gmail.com>
2013-05-01 10:20:11 +0200

    Deprecated custom Web font

--- app/views/global.blade.php
+++ app/views/global.blade.php
@@ -3,7 +3,6 @@
 <head>
 	<meta charset="UTF-8">
 	<title>IRCLogs</title>
-		<link href="http://fonts.googleapis.com/css?family=Ubuntu+Mono:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic" rel="stylesheet" type="text/css">
 	<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
 	<link rel="stylesheet" href="app/css/styles.css">
 </head>
