875ab6033afc783af7935ee8e5518521ad2556da
Maxime Fabre <ehtnam6@gmail.com>
2013-05-01 18:27:16 +0200

    Tweak timeline view

--- app/views/partials/logs.blade.php
+++ app/views/partials/logs.blade.php
@@ -8,20 +8,15 @@
 				<li class="log-secondary">
 					–– {{ $log->getDay() }}
 				</li>
-				<li class="log-entry log-entry-{{ $log->type }}">
-					<span class="log-entry-time">
-						{{ $log->getHour() }}
-					</span>
-					{{ $log->getText() }}
-				</li>
-			</a>
-		@else
+		@endif
 			<li class="log-entry log-entry-{{ $log->type }}" data-url="{{ $log->getUrl() }}" id="log-{{ $log->_id }}">
 				<span class="log-entry-time">
 					{{ $log->getHour() }}
 				</span>
 				{{ $log->getText() }}
 			</li>
+		@if (isset($search) && $search)
+			</a>
 		@endif
 	@endforeach
 	@if (isset($moredown))
--- app/views/partials/timeline.blade.php
+++ app/views/partials/timeline.blade.php
@@ -5,7 +5,7 @@
 			<ul>
 				@foreach($months as $month => $days)
 					<li class="timeline-month">
-						<a href="#">{{ $month }}</a>
+						<a href="#">{{ Carbon::createFromDate(2012, $month)->format('F') }}</a>
 						<ul>
 							@foreach($days as $day => $date)
 								@if (Str::contains(URL::full(), $date->format('Y-m-d')))
@@ -14,7 +14,7 @@
 									<li class="timeline-day">
 								@endif
 									<a href="{{ URL::to($date->format('Y-m-d')) }}">
-										{{ $date->format('l, d F y') }}
+										{{ $date->format('d l') }}
 									</a>
 								</li>
 							@endforeach
