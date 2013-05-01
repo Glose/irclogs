27e8baf762ed4c72326325fc583beb0d1f7d5240
Maxime Fabre <ehtnam6@gmail.com>
2013-05-01 16:25:25 +0200

    Remove unused link to time

--- app/routes.php
+++ app/routes.php
@@ -36,7 +36,7 @@ View::composer('partials.timeline', function($view) {
 	// Loop and add to timeline
 	while (strtotime($firstDate) <= strtotime($lastDate)) {
 		list($year, $month, $day) = explode('-', $firstDate);
-		$timeline[$year][$month][$day] = URL::to($year.'-'.$month.'-'.$day. '/00:00');
+		$timeline[$year][$month][$day] = URL::to($year.'-'.$month.'-'.$day);
 
 		$firstDate = date ("Y-m-d", strtotime("+1 day", strtotime($firstDate)));
 	}
