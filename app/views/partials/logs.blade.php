3a23cd184d6cfe92516a46b98ea6ec0f1de3a6fa
Julien Chaumond <chaumond@gmail.com>
2013-05-01 10:52:47 +0200

    Tweak style for search results

--- app/views/partials/logs.blade.php
+++ app/views/partials/logs.blade.php
@@ -1,9 +1,11 @@
 @foreach ($logs as $log)
+  @if (isset($search) && $search)
+    <li class="log-secondary"> 
+      –– {{ $log->getDay() }}
+    </li>
+  @endif
   <li class="log-entry log-entry-{{ $log->type }}">
     <span class="log-entry-time">
-      @if (isset($search) && $search)
-        {{ $log->getDay() }} –
-      @endif
       {{ $log->getHour() }}
     </span>
     {{ $log->getText() }}
