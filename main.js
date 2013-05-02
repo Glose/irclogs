6bf2651517c06ca7132636a34123397deb0b62ee
Julien Chaumond <chaumond@gmail.com>
2013-05-02 21:41:31 +0200

    Send notif to HipChat as well

--- main.js
+++ main.js
@@ -14,6 +14,14 @@ var client = new irc.Client('irc.freenode.net', 'irclogs', {
 
 var logs = mongojs('irclogs').collection('logs');
 
+var Hipchat = require('node-hipchat');
+var hip = new Hipchat('21d0bf9ce0e82f39de024c4d455433');
+
+
+////////////////////////////////////////////////////////////////////
+////////////////////////////// LISTENERS ///////////////////////////
+////////////////////////////////////////////////////////////////////
+
 var logSave = function(hash) {
 	hash.time = new Date;
 	logs.save(hash, {w: 0});
@@ -22,8 +30,9 @@ var logSave = function(hash) {
 client.addListener('message', function (nick, to, text, message) {
 	logSave({type: 'message', nick: nick, text: text});
 	
-	// Send Push notification to iPhone:
+	// Send Push notification to iPhone and HipChat:
 	if (text.indexOf('mongo') !== -1) {
+		hip.postMessage({room: 'reaaad', from: 'irclogs', message: nick+': '+text, color: 'gray'});
 		push.send("irclogs", text);
 	}
 });
